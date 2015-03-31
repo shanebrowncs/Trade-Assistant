7a9b990046f80bd198b5d7791e5c99edaed3cd48
Shane Brown <contact@shane-brown.ca>
2015-03-31 17:08:46 -0400

    Added info addition to How-To page.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 <body>
 	<div id="navbar">
-		<a href="help.html">How To</a>
+		<a href="help.html">How To / Info</a>
 		<a href="#">Settings</a>
 		<a href="#">Item Calculator</a>
 		<a href="index.php">Home</a>
