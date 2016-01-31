2e25007b74398b440f0b136359d8cf26d9bb25b0
Shane Brown <contact@shane-brown.ca>
2016-01-31 15:35:28 -0500

    changed jquery request to https

--- settings.php
+++ settings.php
@@ -41,7 +41,7 @@ if(isset($_POST['currency']) && !empty($_POST['currency'])){
 <html>
 
 <head>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="layout.css" />
 	<link rel="stylesheet" type="text/css" href="settings.css" />
 	<link rel="icon" type="image/png" href="/images/favicon.png" />
